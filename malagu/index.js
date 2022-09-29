6ea0c3f042c1e6b3e51e1e7d351eaa1b57542109
bingtsingw <bingtsingw@gmail.com>
2022-09-29 17:55:35 +0800

    fix: remove prisma command in malagu task

--- malagu/index.js
+++ malagu/index.js
@@ -1,15 +1,9 @@
-const { chmodSync, existsSync } = require('fs');
+const { chmodSync } = require('fs');
 const { packageJson, template } = require('mrm-core');
 const { join } = require('path');
 
 const task = async () => {
-  const { execa } = await import('execa');
-
   packageJson().setScript('build', 'malagu build -m fc').setScript('deploy', './scripts/deploy.sh').save();
-
-  if (!existsSync('prisma')) {
-    await execa('npx prisma init', { shell: true });
-  }
 
   const diff = 'scripts/deploy.sh';
   template(diff, join(__dirname, '../templates/malagu/deploy.sh')).apply().save();
--- templates/malagu/deploy.sh
+++ templates/malagu/deploy.sh
@@ -4,4 +4,7 @@ set -o allexport
 source .env.malagu.local set
 set +o allexport
 
+if [ -f '.env.deploy.local' ]; then
+  cp .env.deploy.local .malagu/dist/backend/.env
+fi
 malagu deploy -m fc -s
